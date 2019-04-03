d9ed4b704831eae48d9dbbedf2287f5583015030
Amit Narang <anarang1@stuy.edu>
2019-04-03 09:23:50 -0400

    sofar

--- util/data.py
+++ util/data.py
@@ -1,20 +1,42 @@
-# user picks a district
-# and different crimes show up for each month
-# too many crimes to choose from, so made it for three different ones
-# Drugs/Possession/Marijuana, Disorderly/Vagrancy/Begging, Murder & Non-Negligent Murder
-# will return a dictionary with keys as districts and values as dictionaries
-# the keys of the nested dict will be months, and their values will be a list of #'s
+import copy
+
+def addMonth():
+    with open("data/Raleigh_Police_Incidents_SRS.csv") as file:
+        lineRead = file.readline()
+        lineRead = file.readline()
+        with open("data/withMonth.csv","w") as writer:
+            while lineRead:
+                lineReadSplit = lineRead.split(",")
+                date = lineReadSplit[-5]
+                month = date.split("-")
+                val = month[1]
+                lineReadSplit[-2] = val
+                toAdd = ",".join(lineReadSplit)
+                writer.write(toAdd)
+                lineRead = file.readline()
+
 def getInfo():
     districts = ['SOUTHEAST','NORTHWEST','NORTH','SOUTHWEST','NORTHEAST','DOWNTOWN']
+    calendar = ["01","02","03","04","05","06","07","08","09","10","11","12"]
+    nestDict1 = dict.fromkeys(calendar)
+    nestDict2 = copy.deepcopy(nestDict1)
+    nestDict3 = copy.deepcopy(nestDict1)
+    nestDict4 = copy.deepcopy(nestDict1)
+    nestDict5 = copy.deepcopy(nestDict1)
+    nestDict6 = copy.deepcopy(nestDict1)
     d = dict.fromkeys(districts)
-    s= set()
-    with open("data/Raleigh_Police_Incidents_SRS.csv","r") as file:
+    d['SOUTHEAST'] = nestDict1
+    d['NORTHWEST'] = nestDict2
+    d['NORTH'] = nestDict3
+    d['SOUTHWEST'] = nestDict4
+    d['NORTHEAST'] = nestDict5
+    d['DOWNTOWN'] = nestDict6
+    with open("data/withMonth.csv","r") as file:
         line = file.readline()
         line = file.readline()
         while line:
             list_line = line.split(",")
-            s.add([list_line[3],list_line[2]])
-            '''
+
             district = list_line[-3]
             if (district in d):
 
@@ -22,10 +44,16 @@ def getInfo():
             else:
                 l = []
                 d[district] = l
-            '''
             line = file.readline()
     return s
 
+# user picks a district
+# and different crimes show up for each month
+# too many crimes to choose from, so made it for three different ones
+# Drugs/Possession/Marijuana, Disorderly/Vagrancy/Begging, Murder & Non-Negligent Murder
+# will return a dictionary with keys as districts and values as dictionaries
+# the keys of the nested dict will be months, and their values will be a list of #'s
+'''
 crimes = getInfo()
 print(crimes)
-    # returns dictionary with nested dictionary
+'''
